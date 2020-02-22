125bf750d75a750be1a48e439aa13f69e3016b59
Ronal Portillo <ronal203010@gmail.com>
2020-02-22 14:16:38 -0800

    02/22/20
    new commit adds a MakemeFile among others

--- Labs/Lab1/lab1.c
+++ Labs/Lab1/lab1.c
@@ -1,7 +1,27 @@
+#define _XOPEN_SOURCE 700
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
+#include <sys/types.h>
+//#include <sys/wait.h> 
+#include <unistd.h>
+
+void printShit(int i);
 
 int main(int argv,char * args[]){
-	printf("Hello, Wolrd!\n");
+	time_t start, stop; 
+
+	start = time(NULL); //start the clock
+	printf("START: %ld\n",start);
+
+	if(fork() == 0){
+		printf("\nI am Child!\n");
+	}else{
+		printf("\nI am  parent!\n");
+	}
+
+	stop = time(NULL);
+	printf("STOP:  %ld\n",stop);
+
 	return 0;
 }//main
